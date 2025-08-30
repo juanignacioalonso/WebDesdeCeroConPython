b3c67a7c458f48afb52426ea9b70ac4c1d49a837
juanignacioalonso <zalbak87@gmail.com>
2025-08-30 13:53:52 -0300

    CORS

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -3,6 +3,10 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio",
     antd_mode=True,
+    cors_allowed_origins = [
+        "http://localhost:3000",
+        "https://link-bio-teal-apple.reflex.run/"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
