f84c7c3be8227105169c78c0c1b08c936b2156dd
juanignacioalonso <zalbak87@gmail.com>
2025-08-30 14:57:42 -0300

    Construccion del frontend en remoto

--- link_bio/local_build.sh
+++ link_bio/local_build.sh
@@ -1,9 +1,9 @@
-.venv/Scripts/activate
+.venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=http://api.juandev.lat reflex export --frontend-only
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
---
+++ link_bio/remote_build.sh
@@ -0,0 +1,11 @@
+cd link_bio
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL=http://api.juandev.lat reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
